ace085bbce3ca0232008c657142eb6833fb0a882
Lynda Herkat <herkatlynda@hotmail.com>
2019-07-16 14:05:07 +0200

    connexion de 2 joueurs a la room game

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -1,26 +1,37 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', function () {
-
+    $('p.wait').hide();
     var socketClient = io();
 
-    console.log('coucou');
-    
-    socketClient.on('playersList', function(data){
+    socketClient.on('playersAvailableList', function(data){
         console.log("TCL: data", data);
+
         $('div#players-connected p').remove();
         
         data.forEach(function(elmt) {
-            console.log('tour de manège')
+            console.log('tour de manège');
             var pPlayerName = $("<p></p>").text(elmt);
             $('div#players-connected').append(pPlayerName);
         });
     });
 
+    //Rejoindre une partie
+    $('a.ready').click(function(e){
+        e.preventDefault();
+        socketClient.emit('ready');
+    });
 
+    socketClient.on('wait', function(){
+        $('p.wait').show();
+    });
 
-
-
+    socketClient.on('letsgo', function(){
+        $('p.wait').hide();
+        window.location.href = "/quiz/game";
+        // $('a.ready').click();
+        // console.log('C\'est dartyyyyy mon kiki !!!');
+    });
 
 
 });
--- server.js
+++ server.js
@@ -13,12 +13,14 @@ const MongoClient = mongodb.MongoClient;
 const objectId = require('mongodb').ObjectID;
 let dbTools = require('./my_modules/db');
 
-//PUG
+//Pug
 const pug = require('pug');
-//PARSER
+
+//Parser
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-//SESSION
+
+//Session
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const sessionMiddleware = session({
@@ -36,8 +38,6 @@ const sessionMiddleware = session({
         ttl: 7 * 24 * 60 * 60 // 7jours
     })
 });
-// const sharedsession = require("express-socket.io-session"); //module partage de session entre socket.io et express (req.session)
-
 
 //Acces aux ressources statiques
 app.use('/public', express.static(__dirname + '/public'));
@@ -48,18 +48,11 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 
-
 //Session
 app.use(sessionMiddleware);
 io.use(function(socket, next){
     sessionMiddleware(socket.request, socket.request.res, next);
 });
-
-
-// io.use(sharedsession(session, { //Permet de partager les sessions entre express et socket.io
-//     autoSave:true
-// }));
-
 
 //Moteur de rendu
 app.set('view engine', 'pug');
@@ -69,13 +62,12 @@ app.use('/', require('./routes/home.js'));
 app.use('/', require('./routes/login-check.js'));
 app.use('/', require('./routes/signin-check.js'));
 app.use('/', require('./routes/quiz-home.js'));
+app.use('/', require('./routes/quiz.js'));
 app.get('/deconnexion', function (req, res) {
     //Destruction de la session
     if (req.session.user_id) {
-
         req.session.destroy();
         res.redirect('/');
-
     } else {
         res.redirect('/');
     }
@@ -109,25 +101,27 @@ app.use(function (err, req, res, next) {
             res.status(500).render('error', dataBox);
         }
     }
-
 });
-
 
 //Ecoute serveur
 myServer.listen(8080, function () {
     console.log('Écoute sur le port 8080');
-    console.log('Joueurs dispo list', playersList);
-    console.log('Joueurs available ', playersAvailable);
+    console.log('playersAvailableList', playersAvailableList);
+    console.log('playersAvailable ', playersAvailable);
 });
-
 
 /************************************************************************/
 /*************************** Sevreur Websocket **************************/
 /************************************************************************/
 
-let playersAvailable = []; // Joueurs connectés
-let playersList = [];
-let playerReadyToPlay = []; // Joueurs voulant jouer
+let playersAvailable = []; // tableau contenant un objet par joueur (caracteristique)
+let playersAvailableList = []; // Joueurs connectes (username)
+let playersReadyToPlayList = []; // Joueurs prets à jouer (userID ?? a definir)
+let roomsList = [];
+let pending_room = null;
+let roomID = function () {
+    return 'room_' + Math.random().toString(36).substr(2, 9);
+};
 let timer = 0;
 
 let Player = function(sessionID, username, user_id, socketID){
@@ -136,38 +130,61 @@ let Player = function(sessionID, username, user_id, socketID){
     this.socketID = socketID;
     this.username = username;
     this.score = 0;
-}
-
-//Socket.io
+};
 
 io.on('connection', function (socketServer) {
 
-    //creation du player 
+    //Creation de l'objet joueur avec toutes ses caracteristiques
+    // et ajout aux tbleaux playersAvailable et playersAvailableList
     let player_sessionID = socketServer.request.sessionID,
         player_username = socketServer.request.session.pseudo,
         player_user_id = socketServer.request.session.user_id,
         player_socketID = socketServer.request.session.ioID;
-    let player_id = 'player' + player_user_id; 
+
+    playersAvailable.push(new Player(player_sessionID, player_username, player_user_id, player_socketID));
 
     //Envoi du tableau des noms de joueurs connectes
-    playersAvailable.push( new Player(player_sessionID, player_username, player_user_id, player_socketID));
     console.log("TCL: playersAvailable", playersAvailable);
     playersAvailable.forEach(function(elmt){
-        if(playersList.indexOf(elmt.username) === -1){
-            playersList.push(elmt.username);
+        if(playersAvailableList.indexOf(elmt.username) === -1){
+            playersAvailableList.push(elmt.username);
         }
     });
-    io.emit('playersList', playersList);
-
-    //Deconnection et Mise à jour des joueuers
+    io.emit('playersAvailableList', playersAvailableList);
+    
+    //Deconnection et Mise à jour du tableau des joueurs connectes
     socketServer.on('disconnect', function(reason){
         
         for(var i = 0; i < playersAvailable.length ; i++){
             if(playersAvailable[i].socketID === player_socketID){
                 playersAvailable.splice(i-1, 1);
-                playersList.splice(playersList.indexOf(player_username), 1);
-                io.emit('playersList', playersList);
+                playersAvailableList.splice(playersAvailableList.indexOf(player_username), 1);
+                io.emit('playersAvailableList', playersAvailableList);
             }
+        }
+        console.log("TCL: playersAvailable apres déconnection", playersAvailable);
+    });
+
+    //Rejoindre une partie
+    socketServer.on('ready', function(){
+        if (pending_room === null){
+            pending_room = roomID();
+            roomsList.push(pending_room);
+        } 
+        socketServer.room = pending_room;
+        socketServer.join(socketServer.room);
+        playersReadyToPlayList.push(player_username);
+        console.log("TCL: playersReadyToPlayList AVANT", playersReadyToPlayList)
+        if (playersReadyToPlayList.length === 2){
+            io.to(socketServer.room).emit('letsgo');
+            pending_room = null; 
+            // remise a zero du tabelau des joueurs en attente de jouer 
+            playersReadyToPlayList = [];
+            console.log("TCL: playersReadyToPlayList APRES", playersReadyToPlayList);
+            console.log("TCL: socketServer.room", socketServer.room);
+            console.log("TCL: playersAvailable apres acces room", playersAvailable)
+        } else {
+            socketServer.emit('wait');
         }
     });
 
