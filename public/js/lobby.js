84e083b0aa2a206b9f0d9a1a0e4588e3fc0bfe08
Lynda Herkat <herkatlynda@hotmail.com>
2019-07-19 21:46:27 +0200

    correction bug connection + single page + chrono

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -1,36 +1,70 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', function () {
+
+    ///////LOBBY////////
     $('p.wait').hide();
     var socketClient = io();
-
-    socketClient.on('playersAvailableList', function(data){
+    //Affichage des joueurs connectes
+    socketClient.on('playersAvailableList', function (data) {
         console.log("TCL: data", data);
 
         $('div#players-connected p').remove();
-        
-        data.forEach(function(elmt) {
+
+        data.forEach(function (elmt) {
             console.log('tour de manège');
             var pPlayerName = $("<p></p>").text(elmt);
             $('div#players-connected').append(pPlayerName);
         });
     });
 
-    //Rejoindre une partie
-    $('a.ready').click(function(e){
+    //Click Rejoindre une partie lobby
+    $('a.ready').click(function (e) {
         e.preventDefault();
         socketClient.emit('ready');
     });
 
-    socketClient.on('wait', function(){
+    //Click deconnection
+    $('a.deconnection').click(function(){
+        socketClient.close();
+    });
+
+    //Attente d'un second joueur lobby
+    socketClient.on('wait', function () {
         $('p.wait').show();
     });
 
-    socketClient.on('letsgo', function(){
+    //Lancement du jeu + decompte + chrono 
+    var clearCounter;
+    var counter = 6;
+    var finish = function () {
+        clearInterval(clearCounter);
+    };
+    var count = function(){
+        counter--;
+        if (counter === -1) {
+            finish();
+            $('p.count').html('GO');
+            socketClient.emit('askChrono');
+        } else {
+            $('p.count').html(counter);
+        }
+    };
+
+    var start = function(){
+        clearCounter = setInterval(count, 1000);
+    };
+
+
+    socketClient.on('letsgo', function () {
         $('p.wait').hide();
-        window.location.href = "/quiz/game";
-        // $('a.ready').click();
-        // console.log('C\'est dartyyyyy mon kiki !!!');
+        $('a.ready').hide();
+        start();
+        // socketClient.emit('askChrono');
+    });
+
+    socketClient.on('sendChrono', function(data){
+        $('p.chrono span.seconds').html(data.time);
     });
 
 
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const sessionMiddleware = session({
     secret: 'jeubackendifocop',
-    name : 'sessionID',
+    name: 'sessionID',
     resave: false, //false : pas de re enregisrtement de la session (preconisé par connect-mongo)
     saveUninitialized: true, //
     cookie: {
@@ -50,7 +50,7 @@ app.use(cookieParser());
 
 //Session
 app.use(sessionMiddleware);
-io.use(function(socket, next){
+io.use(function (socket, next) {
     sessionMiddleware(socket.request, socket.request.res, next);
 });
 
@@ -62,7 +62,7 @@ app.use('/', require('./routes/home.js'));
 app.use('/', require('./routes/login-check.js'));
 app.use('/', require('./routes/signin-check.js'));
 app.use('/', require('./routes/quiz-home.js'));
-app.use('/', require('./routes/quiz.js'));
+// app.use('/', require('./routes/quiz.js'));
 app.get('/deconnexion', function (req, res) {
     //Destruction de la session
     if (req.session.user_id) {
@@ -122,20 +122,21 @@ let pending_room = null;
 let roomID = function () {
     return 'room_' + Math.random().toString(36).substr(2, 9);
 };
-let timer = 0;
 
-let Player = function(sessionID, username, user_id, socketID){
+//Caracteristique joueur
+let Player = function (sessionID, username, user_id, socketID) {
     this.sessionID = sessionID;
-    this.user_id = user_id;  
+    this.user_id = user_id;
     this.socketID = socketID;
     this.username = username;
     this.score = 0;
 };
 
+//Debut de la connection websocket
 io.on('connection', function (socketServer) {
 
     //Creation de l'objet joueur avec toutes ses caracteristiques
-    // et ajout aux tbleaux playersAvailable et playersAvailableList
+    // et ajout aux tableaux playersAvailable et playersAvailableList
     let player_sessionID = socketServer.request.sessionID,
         player_username = socketServer.request.session.pseudo,
         player_user_id = socketServer.request.session.user_id,
@@ -144,48 +145,80 @@ io.on('connection', function (socketServer) {
     playersAvailable.push(new Player(player_sessionID, player_username, player_user_id, player_socketID));
 
     //Envoi du tableau des noms de joueurs connectes
-    console.log("TCL: playersAvailable", playersAvailable);
-    playersAvailable.forEach(function(elmt){
-        if(playersAvailableList.indexOf(elmt.username) === -1){
+    console.log("TCL: playersAvailable lors de la socket connection de ", player_username, playersAvailable);
+    playersAvailable.forEach(function (elmt) {
+        if (playersAvailableList.indexOf(elmt.username) === -1) {
             playersAvailableList.push(elmt.username);
         }
     });
     io.emit('playersAvailableList', playersAvailableList);
-    
+
     //Deconnection et Mise à jour du tableau des joueurs connectes
-    socketServer.on('disconnect', function(reason){
-        
-        for(var i = 0; i < playersAvailable.length ; i++){
-            if(playersAvailable[i].socketID === player_socketID){
-                playersAvailable.splice(i-1, 1);
+    socketServer.on('disconnect', function (reason) {
+        console.log("TCL: reason", reason)
+
+        for (var i = 0; i < playersAvailable.length; i++) {
+            if (playersAvailable[i].socketID === player_socketID) {
+                playersAvailable.splice(i, 1);
                 playersAvailableList.splice(playersAvailableList.indexOf(player_username), 1);
                 io.emit('playersAvailableList', playersAvailableList);
             }
+
         }
-        console.log("TCL: playersAvailable apres déconnection", playersAvailable);
+        console.log("TCL: playersAvailable apres déconnection de ", player_username, playersAvailable);
+        console.log("TCL: playersAvailableList apres déconnection", playersAvailableList);
+
     });
 
     //Rejoindre une partie
-    socketServer.on('ready', function(){
-        if (pending_room === null){
+    socketServer.on('ready', function () {
+        if (pending_room === null) {
             pending_room = roomID();
             roomsList.push(pending_room);
-        } 
+        }
         socketServer.room = pending_room;
         socketServer.join(socketServer.room);
+        console.log("TCL: entree dans la room", socketServer.room)
+
         playersReadyToPlayList.push(player_username);
         console.log("TCL: playersReadyToPlayList AVANT", playersReadyToPlayList)
-        if (playersReadyToPlayList.length === 2){
+        if (playersReadyToPlayList.length === 2) {
+            //Debut de l'echange dans la room
             io.to(socketServer.room).emit('letsgo');
-            pending_room = null; 
-            // remise a zero du tabelau des joueurs en attente de jouer 
+
+            // remise à zero du tableau des joueurs en attente de jouer 
+            pending_room = null;
             playersReadyToPlayList = [];
             console.log("TCL: playersReadyToPlayList APRES", playersReadyToPlayList);
-            console.log("TCL: socketServer.room", socketServer.room);
             console.log("TCL: playersAvailable apres acces room", playersAvailable)
+            console.log("TCL: socketServer.room", socketServer.room);
+            console.log("TCL: roomsList", roomsList);
+
         } else {
             socketServer.emit('wait');
         }
+    });
+
+    //Conf chrono
+    let timer;
+    let seconds = 11;
+    let chrono = function () {
+        timer = setInterval(function () {
+            seconds -= 1;
+            if (seconds >= 0) {
+                io.to(socketServer.room).emit('sendChrono', {
+                    time: seconds
+                });
+            } else {
+                stopChrono();
+            }
+        }, 1000);
+    };
+    let stopChrono = function () {
+        clearInterval(timer);
+    };
+    socketServer.on('askChrono', function () {
+        chrono();
     });
 
     // console.log("socketServer.request.sessionID", socketServer.request.sessionID);
@@ -193,7 +226,7 @@ io.on('connection', function (socketServer) {
     // );
     // console.log("socketServer.handshake.headers", socketServer.handshake.headers
     // );
-    
+
     // console.log("TCL: socketServer.id", socketServer.id)
 
 });
